aad6885ae5ab109c179fb932b7158793eee4f3d5
Youjin Shin <meggonagul@gmail.com>
2014-10-10 12:00:07 -0400

    sewer vs comb

--- public/camb_sewer.js
+++ public/camb_sewer.js
@@ -1,3 +1,17 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoibWVnZ29uYWd1bCIsImEiOiI1cFpUOE5RIn0.jooCCIM584kmRt2nkSOcHw';
+
+// //  maps
+// var map = L.mapbox.map('map', 'examples.map-20v6611k', {
+// 	minZoom: 10,
+// 	maxZoom: 18
+// }).setView([42.3783903,-71.1129096+0.02], 13);
+
+//  maps
+var map = L.mapbox.map('map', {
+	minZoom: 10,
+	maxZoom: 18,
+	color: "black"
+}).setView([42.3783903,-71.1129096], 13);
 
 queue() // upload data using queue 
 	// .defer(d3.json, "sewer_network.geojson")// cambridge drainage data
@@ -13,30 +27,53 @@ function ready(error, data) {
 	console.log("sewer data uploaded");
 	// console.log(data);
 
+	// layer.addTo(map);
+
 	// sewer 
 	L.geoJson(data, {
-		style: function(feature) {
+		style: getStyle
+		// style: function(feature) {
 
-			return {
-				color: "rgba(255,20,20,1)",
-				weight: 1,
-				opacity: 1
-			};
-		}
+		// 	return {
+		// 		color: "rgba(255,20,20,1)",
+		// 		weight: 1,
+		// 		opacity: 1
+		// 	};
+		// }
 	}).addTo(map);
+
+	// var featureLayer = L.mapbox.featureLayer(data)
+	//     // hide all markers
+	//     .setFilter(function(d) { 
+	//     	// console.log(d.properties.WATERTYPE);
+	//     	return d.properties.WATERTYPE === 'COMB'; 
+	//     })
+	//     .addTo(map);
+
+	// map.featureLayer.setFilter(function(f) {
+	//        return f.properties['marker-symbol'] === 'fast-food';
+	//    });
+}
+
+// style for total population
+function getStyle(feature) {
+	var type = feature.properties.WATERTYPE;
+	var c = getColor( type );
+
+	return {
+		weight: 1,
+		opacity: 1,
+		color: c
+		// fillOpacity: 0.85,
+		// fillColor: c
+	};
+}
+
+function getColor(d) {
+	if (d == 'COMB') { return 'red'; }
+	else { return "grey"; }
 }
 
 
-//  maps
-var mbUrl = 'https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png';
-
-var grayscale   = L.tileLayer(mbUrl, {id: 'examples.map-20v6611k'}),
-	streets  = L.tileLayer(mbUrl, {id: 'examples.map-i875mjb7'});
-
-var map = L.map('map', {
-	center: [42.3783903,-71.1129096 - 0.015],
-	zoom: 13,
-	layers: grayscale
-});
 
 
